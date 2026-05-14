2271ac33f304d101afba281298b4dfb2bc80156e
luis-datavspace <fernandomorenoluis123@gmail.com>
2026-05-13 21:06:12 -0600

    feat: actualizar codigo del simulador (pipeline completo v1) y organizar estructura del repositorio

--- P1- SIMULADOR DE SENSORES/main.py
+++ P1- SIMULADOR DE SENSORES/main.py
@@ -5,6 +5,8 @@ from sensor import Sensor
 
 
 
+#TODO ESTO ES UN TIPO DEBUG, NO ES NADA FUNCIONAL PARA EL FINAL PLANEADO, SOLO DE PRUEBA
+
 sistema = Monitor()
 sensor_maq_uno = Sensor("S01","temperatura", "°C", 99)
 lectura = Lectura("S01", 89, "2026-04-24")
@@ -20,3 +22,13 @@ sistema.agregar_sensor(sensor_maq_uno)
 sistema.agregar_sensor(sensor_maq_2)
 sistema.agregar_sensor(sensor_maq_3)
 print(len(sistema.sensores))
+
+sistema.recibir_lectura(sensor_maq_uno, 85)
+sistema.recibir_lectura(sensor_maq_uno, 150)
+sistema.recibir_lectura(sensor_maq_uno, 'Error')
+print(len(sistema.lecturas))
+
+alertas = sistema.detectar_alertas()
+print(len(alertas))
+
+sistema.generar_reporte()
--- P1- SIMULADOR DE SENSORES/monitor.py
+++ P1- SIMULADOR DE SENSORES/monitor.py
@@ -1,3 +1,5 @@
+from lectura import Lectura
+
 class Monitor:
     """
     Sistema central que maneja la recepción de datos y el análisis.
@@ -10,11 +12,38 @@ class Monitor:
 
     def agregar_sensor(self,sensor):
         #Registra un nuevo sensor en el sistema central
+
         self.sensores.append(sensor)
 
     def recibir_lectura(self, sensor, valor):
-        """
-        Punto de entrada para nuevos datos
-        Esta será la parte de la validación
-        """
-        pass
+        #Recibe una lectura y la guarda en un objeto para ser validada y almacenada
+
+        nueva_lectura = Lectura(sensor.sensor_id,valor,"2026-01-01")
+        
+        #La siguiente condicion valida si falla en el tipo de valor necesario para el correcto funcionamiento de la fabrica
+        if not(isinstance(valor, (int,float))):
+            nueva_lectura.es_valida = False
+            
+        self.lecturas.append(nueva_lectura)
+        
+    def detectar_alertas(self):
+        #Recorre la lista de lecturas y encuentra cual o cuales  son las peligrosas (es decir las que no son validas)
+
+        alertas = []
+
+        for lectura in self.lecturas:
+            if lectura.es_valida:
+                for sensor in self.sensores:
+                    if sensor.sensor_id == lectura.sensor_id:
+                        if lectura.valor > sensor.limite_max:
+                            alertas.append(lectura)
+        return alertas
+
+    def generar_reporte(self):
+        #Reporte visual de los datos obtenidos hasta el momento 
+
+        print(f"El numero total de lecturas es de:{len(self.lecturas)}")
+        lecturas_invalidas = len([x for x in self.lecturas if x.es_valida == 0]) #List comprehension para el conteo de las lecturas invalidas
+        print(f"Hubo un total de: {lecturas_invalidas}. Esto representa el {round((lecturas_invalidas/len(self.lecturas)*100))}% de todas las lecturas")
+        print(f"Hay un total de {len(self.detectar_alertas())} alertas registradas.")
+        print(f"El estado actual de la fabrica es: {"normal" if (lecturas_invalidas/len(self.lecturas)*100) < 50 else "en peligro"}")
