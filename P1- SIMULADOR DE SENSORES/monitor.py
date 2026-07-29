1f8cdc5cdb0110dbac4210268018bd0578bc3550
luis-datavspace <fernandomorenoluis123@gmail.com>
2026-07-28 18:08:58 -0600

    feat: V3 completa - sensor con mas errores y timestamps en las alertas

--- P1- SIMULADOR DE SENSORES/monitor.py
+++ P1- SIMULADOR DE SENSORES/monitor.py
@@ -43,14 +43,40 @@ class Monitor:
     def generar_reporte(self):
         #Reporte visual de los datos obtenidos hasta el momento 
 
+        alertas = self.detectar_alertas()
         print(f"El numero total de lecturas es de:{len(self.lecturas)}")
         lecturas_invalidas = len([x for x in self.lecturas if x.es_valida == 0]) #List comprehension para el conteo de las lecturas invalidas
-        print(f"Hubo un total de: {lecturas_invalidas}. Esto representa el {round((lecturas_invalidas/len(self.lecturas)*100))}% de todas las lecturas")
-        print(f"Hay un total de {len(self.detectar_alertas())} alertas registradas.")
+        print(f"Hubo un total de: {lecturas_invalidas} lecturas invalidas. Esto representa el {round((lecturas_invalidas/len(self.lecturas)*100))}% de todas las lecturas")
+        print(f"El sensor con más errores fue el: {self.sensor_con_mas_errores()}")
+        print(f"Hay un total de {len(alertas)} alertas registradas.")
+
+        for alerta in alertas:
+            print(f"- {alerta.sensor_id} | {alerta.timestamp} | {alerta.valor}")
+
+
+
         print(f"El estado actual de la fabrica es: {"normal" if (lecturas_invalidas/len(self.lecturas)*100) < 50 else "en peligro"}")
 
         with open("Reporte.txt", "w") as archivo:
+
             archivo.write(f"El numero total de lecturas es de:{len(self.lecturas)} \n")
             archivo.write(f"Hubo un total de: {lecturas_invalidas}. Esto representa el {round((lecturas_invalidas/len(self.lecturas)*100))}% de todas las lecturas\n")
-            archivo.write(f"Hay un total de {len(self.detectar_alertas())} alertas registradas.\n")
+            archivo.write(f"El sensor con más errores fue el: {self.sensor_con_mas_errores()}")
+            archivo.write(f"Hay un total de {len(alertas)} alertas registradas.\n")
+            for alerta in alertas:
+                    archivo.write(f"- {alerta.sensor_id} | {alerta.timestamp} | {alerta.valor}")
             archivo.write(f"El estado actual de la fabrica es: {"normal" if (lecturas_invalidas/len(self.lecturas)*100) < 50 else "en peligro"}\n")
+
+    def sensor_con_mas_errores(self):
+        conteo = {}
+
+        for lectura in self.lecturas:
+            if not(lectura.es_valida):
+                if lectura.sensor_id in conteo:
+                    conteo[lectura.sensor_id] += 1
+                else:
+                    conteo[lectura.sensor_id] = 1
+
+        sensor_mas_erratico = max(conteo, key=conteo.get)
+
+        return sensor_mas_erratico
